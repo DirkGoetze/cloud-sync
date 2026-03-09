49695129580752b7de619a18bcf87e895f09a9b8
Dirk Götze <goetze.dirk@hotmail.de>
2026-03-09 03:03:33 +0100

    Optimierte Fortschrittsanzeige: unbuffered output, --no-inc-recursive, bessere Filterung

--- lib/cloud-sync.sh
+++ lib/cloud-sync.sh
@@ -131,19 +131,20 @@ watch_and_sync() {
     if [[ -n "$RSYNC_OPTS" ]]; then
         echo "[$(date)] [$JOB_NAME] [INIT] Starte initiale Synchronisation..." >> "$LOG_FILE"
         
-        # Verwende --info=progress2 für Live-Fortschritt und --stats für finale Zusammenfassung
-        RSYNC_OPTS_PROGRESS="${RSYNC_OPTS//-v/} --info=progress2 --stats"
+        # Verwende --info=progress2 für Live-Fortschritt, --no-inc-recursive für sofortige Dateianzahl
+        RSYNC_OPTS_PROGRESS="${RSYNC_OPTS//-v/} --info=progress2 --no-inc-recursive --stats"
         
-        # Führe rsync aus und logge Fortschritt + Stats
-        # xfr# = Fortschrittszeilen, rest = finale Stats
+        # Führe rsync aus mit unbuffered output
         rsync $RSYNC_OPTS_PROGRESS "$SOURCE/" "$TARGET/" 2>&1 | \
-            grep -E "xfr#|ir-chk|to-chk|Number of files|Total file size|Total transferred|speedup" | \
+            stdbuf -oL grep -E "xfr#|ir-chk|to-chk|Number of files|Total file size|Total transferred|speedup" | \
             while IFS= read -r line; do
-                # Fortschrittszeilen (xfr#) nur alle 100 Transfers loggen, um Log nicht zu überfluten
+                # Fortschrittszeilen (xfr#) selektiv loggen
                 if [[ "$line" =~ xfr#([0-9]+) ]]; then
                     XFR_NUM="${BASH_REMATCH[1]}"
-                    # Logge nur bei jedem 100. Transfer oder wenn Zahl auf 00 endet
-                    if [[ $((XFR_NUM % 100)) -eq 0 ]] || [[ "$XFR_NUM" =~ 00$ ]]; then
+                    # Erste 10 immer, dann jedes 50. (bis 1000), ab 1000 jedes 100.
+                    if [[ $XFR_NUM -le 10 ]] || 
+                       [[ $XFR_NUM -lt 1000 && $((XFR_NUM % 50)) -eq 0 ]] || 
+                       [[ $XFR_NUM -ge 1000 && $((XFR_NUM % 100)) -eq 0 ]]; then
                         echo "[$(date)] [$JOB_NAME] [PROGRESS] $line" >> "$LOG_FILE"
                     fi
                 else
